8736b7ff1cf854479d32ef8c547f0f1b63884fb2
LostElk <lostelk.email@gmail.com>
2024-01-21 15:12:37 +0800

    umd/cjs/es模块与ts打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -2,38 +2,70 @@ import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig([
   {
+    // UMD 输出配置
+    input: 'src/index.ts',
+    output: {
+      format: 'umd',
+      name: 'NearestColor',
+      file: 'dist/index.umd.js',
+      inlineDynamicImports: true,
+      globals: {
+        tinycolor2: 'tinycolor',
+      },
+    },
+    plugins: [
+      commonjs({
+        include: /node_modules/,
+      }),
+      typescript(),
+      resolve(),
+      terser(),
+    ],
+  },
+  {
+    // CommonJS (CJS) 输出配置
+    input: 'src/index.ts',
+    output: {
+      format: 'cjs',
+      file: 'dist/index.cjs',
+    },
+    external: ['tinycolor2'],
+    plugins: [
+      typescript(),
+      terser(),
+    ],
+  },
+  {
+    // ES Module (ESM) 输出配置
+    input: 'src/index.ts',
+    output: {
+      format: 'es',
+      file: 'dist/index.mjs',
+    },
+    external: ['tinycolor2'],
+    plugins: [
+      typescript(),
+      terser(),
+    ],
+  },
+  {
+    // Declaration 文件输出配置
     input: 'src/index.ts',
     output: [
       {
-        format: 'umd',
-        name: 'NearestColor',
-        file: 'dist/index.umd.js',
-        globals: {
-          tinycolor2: 'tinycolor',
-        },
-      },
-      {
-        format: 'cjs',
-        file: 'dist/index.cjs.js',
-      },
-      {
+        file: 'dist/index.d.mts',
         format: 'es',
-        file: 'dist/index.esm.js',
       },
+      {
+        file: 'dist/index.d.cts',
+        format: 'cjs',
+      }
     ],
-    plugins: [typescript(), terser()],
-    external: ['tinycolor2'],
-  },
-  {
-    // 打包声明文件
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.d.ts',
-      format: 'es',
-    },
     plugins: [dts()],
   },
 ]);
