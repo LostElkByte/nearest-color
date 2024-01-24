37d488f0542c2589f3ab330723a3ad0f1e96f275
LostElk <lostelk.email@gmail.com>
2024-01-24 10:47:22 +0800

    更新测试用例

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,56 +6,40 @@ import { BASE_COLORS } from '../src/utils/defaultColors';
 describe('NearestColor', () => {
   // Find the nearest color - default color palette
   it('finds the nearest color - default color palette', () => {
-    // Create an instance of the NearestColor class, initialized with the default color palette
     const nearestColorInstance = new NearestColor();
 
     console.time('find nearest color - default color palette');
-    // Call the find method to find the nearest color, test color is #FF5733
     const result = nearestColorInstance.find('#FF5733');
     console.timeEnd('find nearest color - default color palette');
 
-    console.log('Result:', result);
-
-    // Use Jest assertions to ensure that the nearest color is categorized as 'red' with a distance of approximately 23.32
     expect(result.category).toBe('red');
     expect(result.distance).toBeCloseTo(23.323807579381203, 0);
   });
 
   // Find the nearest color - custom color palette
   it('finds the nearest color - custom color palette', () => {
-    // Create an instance of the NearestColor class, initialized with a custom color palette (BASE_COLORS)
     const nearestColorInstance = new NearestColor(BASE_COLORS);
 
     console.time('find nearest color - custom color palette');
-    // Call the find method to find the nearest color, test color is #ffff00
     const result = nearestColorInstance.find('#ffff00');
     console.timeEnd('find nearest color - custom color palette');
 
-    console.log('Result:', result);
-
-    // Use Jest assertions to ensure that the nearest color is categorized as 'yellow'
     expect(result.category).toBe('yellow');
   });
 
   // Find the nearest color - custom color palette using the from method
   it('finds the nearest color - from method', () => {
-    // Create an instance of the NearestColor class using the from method and a custom color palette (BASE_COLORS)
     const nearestColorInstance = NearestColor.from(BASE_COLORS);
 
     console.time('find nearest color - from method');
-    // Call the find method to find the nearest color, test color is #ffff00
     const result = nearestColorInstance.find('#ffff00');
     console.timeEnd('find nearest color - from method');
 
-    console.log('Result:', result);
-
-    // Use Jest assertions to ensure that the nearest color is categorized as 'yellow'
     expect(result.category).toBe('yellow');
   });
 
   // Find the nearest color - concat method to add extra colors to the existing color palette
   it('finds the nearest color - concat method', () => {
-    // Create an instance of the NearestColor class, initialized with the default color palette
     const nearestColorInstance = new NearestColor();
 
     console.time('find nearest color - concat method');
@@ -65,17 +49,34 @@ describe('NearestColor', () => {
         name: ['褐', '褐色'],
       },
     ];
-
-    // Add extra colors to the default or existing color palette
     const nearestColorInstanceNew = nearestColorInstance.concat(MY_COLORS);
-
-    // Call the find method to find the nearest color, test color is #8A8360
     const result = nearestColorInstanceNew.find('#8A8360');
     console.timeEnd('find nearest color - concat method');
 
-    console.log('Result:', result);
-
-    // Use Jest assertions to ensure that the nearest color has the key '#8A8360'
     expect(result.key).toBe('#8A8360');
+  });
+
+  // Find the nearest color - throws an error for invalid color input
+  it('finds the nearest color - throws an error for invalid color input', () => {
+    const nearestColorInstance = new NearestColor();
+    expect(() => nearestColorInstance.find('invalidColor')).toThrow('Invalid color: invalidColor');
+  });
+
+  // Find the nearest color - throws an error on the empty array color palette
+  it('finds the nearest color - throws an error on the empty array color palette', () => {
+    try {
+      const nearestColorInstance = new NearestColor([]);
+      expect(() => nearestColorInstance.find('#FF5733')).toThrow('Color palette must be a non-empty array');
+    } catch (error) {
+    }
+  });
+
+  // Find the nearest color - throws an error on the empty array color palette to from method
+  it('finds the nearest color - throws an error on the empty array color palette to from method', () => {
+    try {
+      const nearestColorInstance = new NearestColor([]);
+      expect(() => nearestColorInstance.find('#FF5733')).toThrow('Color palette must be a non-empty array');
+    } catch (error) {
+    }
   });
 });
